ef74affcee4d5f1a0192a5f53809d5c2492d9f49
JuaneFe14 <jfquinterogu14@gmail.com>
2024-01-16 22:59:25 -0500

    Initial commit

---
+++ resources/views/modules/auth/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/main')
+@section('titulo', 'Login')
+@section('contenido')
+<section class="container">
+    <article class="row">
+        <div class="col">
+            <h2>Iniciar sesión</h2>
+        </div>
+    </article>
+</section>
+@endsection
---
+++ resources/views/modules/listado_seguimiento/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/main')
+@section('titulo', 'Listado')
+@section('contenido')
+<section class="container">
+    <article class="row">
+        <div class="col">
+            <h2>Listado</h2>
+        </div>
+    </article>
+</section>
+@endsection
